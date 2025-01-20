abcaa4a8fb9765c6d7b7f2f394befa3f84542c9a
Imene Bentifraouine <bentifraouineimene@gmail.com>
2025-01-20 14:29:44 +0100

    Initial commit - Add Vue.js

---
+++ nextu-vue-helloworld/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <product-list :products="products" />
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      products: [
+        { id: 1, label: 'Carotte', description: 'De belles carottes', qte: 10 },
+        { id: 2, label: 'Pomme', description: 'Des pommes vertes', qte: 0 },
+        { id: 3, label: 'Tomate', description: 'Tomates rouges', qte: 5 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
